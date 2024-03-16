4c82fbb7c123d3d163bb7030716ce0ead76fa88c
mk-pmb <mk-pmb@users.noreply.github.com>
2024-03-16 20:36:43 +0100

    whatsup: Ignore file permissions when gitfile points to different FS.

--- whatsup.sh
+++ whatsup.sh
@@ -11,15 +11,21 @@ function whatsup_cli_main () {
   COLUMNS="${COLUMNS:-80}"
 
   local GIT_TOPLEVEL="$(git rev-parse --show-toplevel)"
-  local WARNINGS="$(collect_warnings)"
-  ( [ -z "$WARNINGS" ] || echo "$WARNINGS"
+  local GIT_STATUS_OPTS=(
+    -c color.status=always
+    )
+
+  local IMPORTANT_HINTS=()
+  maybe_warn_gitfile || return $?
+
+  ( print_important_hints
     git branch -v
     git-log-concise -n 3 | cut -b 1-$(( $COLUMNS - 1 )) | sed -rf  <(echo '
       s~   +~\t~
       s~^([^\t]+)\t(\S+)\s+~· \f<>\1  \f<purple>\2\f<>  ~g
       1{s~^·~→~g; s~(\f<>)  ~ \f<brown>»\1~}
       ') | lookup_colornames
-    git -c color.status=always status "${GIT_STATUS_ARGS[@]}" | sed -rf <(echo '
+    git "${GIT_STATUS_OPTS[@]}" status "${GIT_STATUS_ARGS[@]}" | sed -rf <(echo '
       1{/^#? ?On branch \S+$/{N
         s~\n#?Your branch is( ahead of )~,\1~
       }}
@@ -35,7 +41,7 @@ function whatsup_cli_main () {
       /^#? +\(use "git checkout -- <file>\.*" to discard changes /d
       s~^#?[^\t\f]*~\f<dimgray>&\f<>~
       ') | lookup_colornames
-    [ -z "$WARNINGS" ] || echo "$WARNINGS"
+    print_important_hints
   ) | smart-less-pmb
   return 0
 }
@@ -64,10 +70,13 @@ function lookup_colornames () {
 }
 
 
-function collect_warnings () {
-  (
-    maybe_warn_gitfile
-  ) | sed -rf <(echo '
+function imphint () { IMPORTANT_HINTS+=( "$*" ); }
+
+
+function print_important_hints () {
+  [ "${#IMPORTANT_HINTS[@]}" -ge 1 ] || return 0
+  # ^-- With no arguments, printf would hallucinate an empty string argument.
+  printf '%s\n' "${IMPORTANT_HINTS[@]}" | sed -rf <(echo '
     s~^W: ~\f<yellow>&~
     ') | lookup_colornames
 }
@@ -82,10 +91,25 @@ function maybe_warn_gitfile () {
     "$HOME" ) GD_HR='~/';;
     "$HOME"/* ) GD_HR='~'"${GD_HR:${#HOME}}";;
   esac
-  [[ "$GITDIR" == */ ]] && echo "W: gitfile path ends in '/'"
-  [[ "$GITDIR" == */.git ]] && echo "W: gitfile points to $GD_HR =>" \
-    "this repo here may have file system limitations that would not apply in" \
-    "${GD_HR%.git}."
+  [[ "$GITDIR" == */ ]] && imphint "W: gitfile path ends in '/'"
+  maybe_warn_gitfile__check_fstype || return $?
+}
+
+
+function maybe_warn_gitfile__check_fstype () {
+  local MSG="H: gitfile points to $GD_HR"
+  local GD_FS="$(stat --file-system -c %T -- "$GITDIR")"
+  local WD_FS="$(stat --file-system -c %T -- .)"
+  [ "$GD_FS" == "$WD_FS" ] || case "$WD_FS -> $GD_FS" in
+    ext[0-9]*' -> 'ext[0-9]* ) ;;
+    * )
+      MSG="W${MSG:1} which uses a different file system ($GD_FS) $(
+        )than this worktree ($WD_FS)."
+      MSG+=" The report thus ignored file permissions."
+      GIT_STATUS_OPTS+=( -c core.filemode=false )
+      ;;
+  esac
+  imphint "$MSG"
 }
 
 
