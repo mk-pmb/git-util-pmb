2e1ebe39e4b32ac1022966c96e8733657a997c6a
mk-pmb <mk-pmb@users.noreply.github.com>
2025-01-11 04:15:29 +0100

    reex: Deal with very limited busybox.

--- reex.sh
+++ reex.sh
@@ -8,19 +8,27 @@ function hard_reset_experimental () {
   local REPO_DIR="$(git rev-parse --show-toplevel)"
   [ -d "$REPO_DIR" ] || return 2$(echo E: 'Failed to find git work tree!' >&2)
   cd -- "$REPO_DIR"
-  local OWNER="$(stat -c %U .)"
-  local SUDO=
-  [ "$USER" == "$OWNER" ] || SUDO="sudo -u $OWNER"
+
+  local OWNER= SUDO=
+  if stat --help 2>&1 | grep -qPe ' -c[, ]'; then
+    # ^-- Busybox on some WD NAS only has -L and -t
+    OWNER="$(stat -c %U .)"
+    [ "$USER" == "$OWNER" ] || SUDO="sudo -u $OWNER"
+  fi
+
   $SUDO git stash
-  $SUDO git fetch origin
+  local RMT='origin'
+  echo "D: fetch: $(git remote -v | grep -Pe "^$RMT\s" | grep -vFe '(push)')"
+  $SUDO git fetch $RMT
+  echo D: checkout:
   $SUDO git checkout -b experimental 2>/dev/null || true
   echo
   echo 'Relevant part starts here.' \
     'Ideally it should say "HEAD is now at" + relevant result.'
-  $SUDO git stash list | sed -rf <(echo '
+  $SUDO git stash list | sed -re '# not using <() for busybox compat.
     1s~^~\nW: Some uncommitted local changes have been stashed:\n~
-    $s~$~\n~')
-  $SUDO git reset --hard origin/experimental
+    $s~$~\n~'
+  $SUDO git reset --hard $RMT/experimental
 }
 
 
