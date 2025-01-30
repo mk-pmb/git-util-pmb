abc4c3a0d7a0e24e10c26a21eedb2d08ef5c4f6b
mk-pmb <mk-pmb@users.noreply.github.com>
2025-01-30 21:33:05 +0100

    v0.1.1: reex: Support custom remote and branch name.

--- reex.sh
+++ reex.sh
@@ -4,7 +4,15 @@
 
 function hard_reset_experimental () {
   export LANG{,UAGE}=en_US.UTF-8  # make error messages search engine-friendly
+  local LOCAL_BRANCH="${1:-experimental}"; shift
+  local RMT="${1:-origin}"; shift
+
+  # split fetch_from:fetch_dest = remote_branch:local_branch
+  local REMOTE_BRANCH="${LOCAL_BRANCH%%:*}"
+  local LOCAL_BRANCH="${LOCAL_BRANCH##*:}"
+
   set -o pipefail -o errexit
+
   local REPO_DIR="$(git rev-parse --show-toplevel)"
   [ -d "$REPO_DIR" ] || return 2$(echo E: 'Failed to find git work tree!' >&2)
   cd -- "$REPO_DIR"
@@ -15,20 +23,20 @@ function hard_reset_experimental () {
     OWNER="$(stat -c %U .)"
     [ "$USER" == "$OWNER" ] || SUDO="sudo -u $OWNER"
   fi
+  [ "${DEBUGLEVEL:-0}" -lt 2 ] || SUDO="echo X: $SUDO"
 
   $SUDO git stash
-  local RMT='origin'
   echo "D: fetch: $(git remote -v | grep -Pe "^$RMT\s" | grep -vFe '(push)')"
-  $SUDO git fetch $RMT
+  $SUDO git fetch "$RMT"
   echo D: checkout:
-  $SUDO git checkout -b experimental 2>/dev/null || true
+  $SUDO git checkout -b "$LOCAL_BRANCH" 2>/dev/null || true
   echo
   echo 'Relevant part starts here.' \
     'Ideally it should say "HEAD is now at" + relevant result.'
   $SUDO git stash list | sed -re '# not using <() for busybox compat.
     1s~^~\nW: Some uncommitted local changes have been stashed:\n~
     $s~$~\n~'
-  $SUDO git reset --hard $RMT/experimental
+  $SUDO git reset --hard "$RMT/$REMOTE_BRANCH"
 }
 
 
