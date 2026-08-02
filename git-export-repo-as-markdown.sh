cee4d60113c6c02a04ed2b0d4414220dfa8b1fef
mk-pmb <mk-pmb@users.noreply.github.com>
2026-08-02 23:52:52 +0200

    git-export-repo-as-markdown: Warn if we export uncommitted changes.

--- git-export-repo-as-markdown.sh
+++ git-export-repo-as-markdown.sh
@@ -6,14 +6,33 @@ function repomd_cli_init () {
   export LANG{,UAGE}=en_US.UTF-8  # make error messages search engine-friendly
   local VAL="$(git rev-parse --show-cdup)"
   [ -z "$VAL" ] || cd -- "$VAL" || return $?
-  exec < <(git ls-files --full-name | sort --version-sort)
+  local GIT_LS_FILES=() MDTB_OPT=()
+  local ARG= KEY= VAL=
+  while [ "$#" -ge 1 ]; do
+    ARG="$1"; shift
+    case "$ARG" in
+      -* ) MDTB_OPT+=( "$ARG" );;
+      * ) GIT_LS_FILES=( "$ARG" );;
+    esac
+  done
+  exec < <(git ls-files --full-name -- "${GIT_LS_FILES[@]}" |
+    sort --version-sort)
   while IFS= read -r VAL; do
     [ -f "$VAL" ] || continue
     set -- "$@" "$VAL"
+    KEY="$(git status --short -uall -- "$VAL")"
+    # ^-- Column 1 is how the staging area differs from the HEAD commit.
+    #     Column 2 is how the worktree differs from the staging area.
+    #     Column 3 is always a space character.
+    KEY="${KEY:0:2}"
   done
+  [ -n "$1" ] || return 4$(echo E: 'Found no files to export!' >&2)
+  local LEAK="$(git status --short -uall -- "$@")"
+  LEAK="${LEAK//$'\n'/, }"
+  [ -z "$LEAK" ] || echo W: "Exporting uncommitted changes: $LEAK" >&2
   echo -ne '\xEF\xBB\xBF'
-  ghciu --ignore-local-config -Q \
-    fmt_markdown_textblock__bundle_files "$@" || return $?
+  ghciu-fmt-markdown-textblock bundle_files "${MDTB_OPT[@]}" "$@" || return $?
+  [ -z "$LEAK" ] || echo W: "Exported uncommitted changes: $LEAK" >&2
 }
 
 
